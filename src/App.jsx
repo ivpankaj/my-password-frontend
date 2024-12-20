b1361fd3209ecce1e53b0bec30946a6b83b51711
Pankaj <imvpankaj@gmail.com>
2024-12-20 23:11:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PasswordManager from './components/PasswordManager';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <PasswordManager />
+    </div>
+  );
+}
+
+export default App;
