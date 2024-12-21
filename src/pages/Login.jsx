37546a694fcd9813e8871d2a7e2e8c4624a6155b
Pankaj <imvpankaj@gmail.com>
2024-12-21 13:29:39 +0530

    asd

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/login", {
+      const response = await axios.post("https://my-password-backend.onrender.com/api/auth/login", {
         email,
         password,
       });
